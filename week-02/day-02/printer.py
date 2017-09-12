397e94f430df5713bd64634638fb075c5022de6c
Borbala Dobo <dobori@gmail.com>
2017-09-12 12:04:04 +0200

    printer(feat): ready

---
+++ week-02/day-02/printer.py
@@ -0,0 +1,8 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer(*input):
+        print(input)
+
+printer(28, 65, 48, 26, 'ablak')
