5defb900bde8fdef1acc862dab1056f93f24f86b
Borbala Dobo <dobori@gmail.com>
2017-09-12 11:04:04 +0200

    greet(feat): ready

---
+++ week-02/day-02/greet.py
@@ -0,0 +1,11 @@
+# - Create variable named `al` and assign the value `Greenfox` to it
+# - Create a function called `greet` that greets it's input parameter
+#     - Greeting is printing e.g. `Greetings, dear Greenfox`
+# - Greet `al`
+
+al = 'Greeenfox'
+
+def greet():
+    print('Greetings, dear ' + al)
+
+greet()
