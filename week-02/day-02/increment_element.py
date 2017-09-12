f63b3c0b9510b2a39cacafdd1e367846be755be9
Borbala Dobo <dobori@gmail.com>
2017-09-12 13:31:35 +0200

    increment_element(feat): ready

---
+++ week-02/day-02/increment_element.py
@@ -0,0 +1,12 @@
+# - Create a variable named `t`
+#   with the following content: `[1, 2, 3, 4, 5]`
+# - Increment the third element
+# - Print the third element
+
+t = [1, 2, 3, 4, 5]
+
+def increment():
+    inc = t[2]+1
+    print(inc)
+
+increment()
