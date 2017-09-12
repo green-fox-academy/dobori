626c5b54081caae059cbec2e1494bfe7379fe03c
Borbala Dobo <dobori@gmail.com>
2017-09-12 12:25:10 +0200

    sum_element(feat): ready

---
+++ week-02/day-02/sum_elements.py
@@ -0,0 +1,14 @@
+# - Create a variable named `r`
+#   with the following content: `[54, 23, 66, 12]`
+# - Print the sum of the second and the third element
+
+r = [54, 23, 66, 12]
+
+def findsum():
+    first = 1
+    last = 2
+    total = 0
+    total = r[first] + r[last]
+    print(total)
+
+findsum()
