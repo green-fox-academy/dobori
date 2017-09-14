20b94d57f657ac35ed34d56e4058b028b482c6a5
Borbala Dobo <dobori@gmail.com>
2017-09-14 16:26:50 +0200

    feat(draw_square):ready - without input, just becouse the easy testing

--- week-02/day-01/draw_square.py
+++ week-02/day-01/draw_square.py
@@ -10,3 +10,13 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+
+
+a = 10
+char = '%'
+empty = ' '
+for i in range(a):
+    if i == 0 or i == a - 1:
+        print( (a-1) * char)
+    else:
+        print( char + (a - 3) * empty + char)
