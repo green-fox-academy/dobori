0bfc88c9b808d5700600a25b7492bfb83b078269
Borbala Dobo <dobori@gmail.com>
2017-09-14 10:46:49 +0200

    feat(cuboid):ready

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,13 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+
+a = 3
+b = 8
+c = 5
+
+surface = 2 *( a * b + a * c + b * c)
+volume = a * b * c
+
+print ('Surface area: ' + str(surface))
+print ('Volume: ' + str(volume))
