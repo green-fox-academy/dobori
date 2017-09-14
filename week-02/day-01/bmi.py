194be21091869d7f0b6e69bd683d4c5388c710b8
Borbala Dobo <dobori@gmail.com>
2017-09-14 10:20:29 +0200

    feat(bmi):ready

--- week-02/day-01/bmi.py
+++ week-02/day-01/bmi.py
@@ -2,3 +2,18 @@ massInKg = 81.2
 heightInM = 1.78
 
 #Print the Body mass index (BMI) based on these values
+
+bmi = 0
+bmi = massInKg / heightInM**2
+print(bmi)
+result =''
+if bmi < 18.5:
+    result = 'You are underweight'
+elif 18.5 <= bmi < 25:
+    result = 'Normal' 
+elif 25 <= bmi < 30:
+    result = 'Overweight'
+else:
+    result = 'Obese'
+
+print(result)
