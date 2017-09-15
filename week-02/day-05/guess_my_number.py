0aac32edd52f1354f504cfb8aaf998054a3cab98
Borbala Dobo <dobori@gmail.com>
2017-09-15 14:32:12 +0200

    feat(guess): ready

---
+++ week-02/day-05/guess_my_number.py
@@ -0,0 +1,41 @@
+from random import randint
+
+# Write a program where the program chooses a number between 1 and 100. The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback and ask to enter an other guess until the guess is correct.
+# Make the range customizable (ask for it before starting the guessing).
+# You can add lives. (optional)
+
+
+is_number = randint(0, 100)
+step_number = 5
+guess = 0
+i = 0
+
+
+def check_feedback(is_number, guess):
+    result = False
+    if guess == is_number:
+        result = True
+        print('You find the number!')
+    elif guess < is_number:
+        result = False
+        print('The number is bigger than you think!')
+    elif guess > is_number:
+        result = False
+        print('The number is smaller than you think!')
+    return(result)
+
+
+print('I think of a number! Guess it: ')
+print(is_number)
+
+while i < step_number:
+    guess = int(input('Your guess is: '))
+    result = check_feedback(is_number, guess)
+    i += 1
+    if i < step_number and result:
+        print('You won!')
+        break
+    elif i == step_number:
+        print('You lose')
+        break
+
