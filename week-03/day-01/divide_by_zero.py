1c346a294d3329b640344fb35e64fc80a4790ffc
Borbala Dobo <dobori@gmail.com>
2017-09-18 11:53:09 +0200

    fix(divide_by_zero.py): bug fixes

--- week-03/day-01/divide_by_zero.py
+++ week-03/day-01/divide_by_zero.py
@@ -3,10 +3,13 @@
 # and prints the result.
 # it should print "fail" if the parameter is 0
 
-num = input('The number is: ')
-devisor = 10
-try:
-    total =  num / devisor
-    print(total)
-except ZeroDivisionError:
-    print('Result can/'t be 0')
+devisor = int(input("The number is: "))
+num = 10
+def divides(devisor):
+    try:
+        total =  num / devisor
+        print(total)
+    except ZeroDivisionError:
+        print("Result can\'t be zero")
+
+divides(devisor)
