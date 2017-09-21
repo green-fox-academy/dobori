a9d90e7dc4e814dadd03540f1df26859a39c6284
Borbala Dobo <dobori@gmail.com>
2017-09-21 13:08:52 +0200

    fix(string2.py): better solution

--- week-03/day-04/string2.py
+++ week-03/day-04/string2.py
@@ -1,11 +1,11 @@
 # Given a string, compute recursively a new string where all the 'x' chars have been removed.
 
-def replaceChar(text, old, new):
+def replaceChar(text, old):
     if text == '':
         return ''
     if text[0] == old:
-        return new + replaceChar(text[1:], old, new)
-    return text[0] + replaceChar(text[1:], old, new)
+        return replaceChar(text[1:], old)
+    return text[0] + replaceChar(text[1:], old)
 
 
-print(replaceChar('tele vagyunk x-ekkelxxx', 'x', ''))
+print(replaceChar('tele vagyunk x-ekkelxxx', 'x'))
