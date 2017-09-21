9e89c2e630963bfac6df6deca788e1d8af5552ce
Borbala Dobo <dobori@gmail.com>
2017-09-21 10:25:06 +0200

    fix(counting): printing out is fixed

--- week-03/day-04/counter.py
+++ week-03/day-04/counter.py
@@ -1,9 +1,10 @@
 # Write a recursive function that takes one parameter: n and counts down from n.
 
-def summa(n):
+def counting(n):
     if n <= 0:
         return 0
     else:
-        return n + summa(n - 1)
+        print(n)
+        return counting(n - 1)
 
-print(summa(40))
+print(counting(40))
