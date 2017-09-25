a9b923528881449e2a80b628157be86a9b2c3a7e
Borbala Dobo <dobori@gmail.com>
2017-09-25 11:45:28 +0200

    feat(postit): make 3 object ready

---
+++ week-04/day-01/post_it.py
@@ -0,0 +1,14 @@
+class PostIt(object):
+
+    def __init__(self, background_color, text, text_color):
+        self.background_color = background_color
+        self.text = text
+        self.text_color = text_color
+
+post_it1 = PostIt("orange", "Idea 1", "blue")
+post_it2 = PostIt("pink", "Awesome", "black")
+post_it3 = PostIt("yellow", "Superb!", "green")
+
+print(post_it1.background_color, post_it1.text, post_it1.text_color)
+print(post_it2.background_color, post_it2.text, post_it2.text_color)
+print(post_it3.background_color, post_it3.text, post_it3.text_color)
