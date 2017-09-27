2893150351becafe6ee48129b4edc41929b4e2f2
Borbala Dobo <dobori@gmail.com>
2017-09-27 17:02:04 +0200

    fix(extend): median function is fixed

--- week-04/day-03/extend.py
+++ week-04/day-03/extend.py
@@ -5,14 +5,23 @@ def add(a, b):
 
 # Returns the highest value from the three given params
 def max_of_three(a, b, c):
-    if a > b:
+    if a > b and a > c:
         return a
+    elif b > a and b > c:
+        return b
     else:
         return c
 
 # Returns the median value of a list given as param
 def median(pool):
-    return pool[int((len(pool) - 1) / 2)]
+    pool.sort()
+    half = len(pool) // 2
+    if len(pool) % 2 ==  0:
+        num = (pool[half] + pool[half - 1]) / 2
+    else:
+        num = pool[half]
+    return num
+
 
 # Returns true if the param is a vovel
 def is_vovel(char):
