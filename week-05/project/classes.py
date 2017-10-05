254617346a43ee06d80ae7dc6f0c594cefaac77e
Borbala Dobo <dobori@gmail.com>
2017-10-05 15:07:13 +0200

    refactor(classes.py)

--- week-05/project/classes.py
+++ week-05/project/classes.py
@@ -1,23 +1,13 @@
 from tkinter import *
 import random
 
-
-width = 720
-height =720
-
-root = Tk()
-
-canvas = Canvas(root, 
-           width=width, 
-           height=height)
-
-canvas.pack()
-
 size = 72
 
-
 class Map(object):
+
+    root = Tk()
     def __init__(self):
+        self.canvas = Canvas(self.root, width = 720, height = 720)
         self.tilemap = [[0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
                         [0, 0, 0, 1, 0, 1, 0, 1, 1, 0],
                         [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
@@ -28,7 +18,6 @@ class Map(object):
                         [0, 0, 0, 0, 0, 1, 1, 0, 1, 0],
                         [0, 1, 1, 1, 0, 0, 0, 0, 1, 0],
                         [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
-
         self.floor = PhotoImage(file = "images/floor.png")
         self.wall = PhotoImage(file = "images/wall.png")
 
@@ -37,9 +26,10 @@ class Map(object):
         for y in range(len(self.tilemap)):
             for x in range(len(self.tilemap)):
                 if self.tilemap[x][y] == 0: 
-                    canvas.create_image(size / 2 + y * size, size / 2 + x * size, image = self.floor)   
+                    self.canvas.create_image(size / 2 + y * size, size / 2 + x * size, image = self.floor)   
                 if self.tilemap[x][y] == 1: 
-                    canvas.create_image(size / 2 + y * size, size / 2 + x * size, image = self.wall) 
+                    self.canvas.create_image(size / 2 + y * size, size / 2 + x * size, image = self.wall) 
+
 
     def get_cell(self, x, y):
         if 0 <= x <= 9 and 0 <= y <= 9:
@@ -48,9 +38,12 @@ class Map(object):
 
 
 class Entity(object):
-    def __init__(self):
-        self.x = x
-        self.y = y
+    def __init__(self, canvas):       
+        self.canvas = canvas
+
+    def random_coords(self):
+        self.x_coord = random.randint(0, 9)
+        self.y_coord = random.randint(0, 9)
 
 
 class Hero(Entity):
@@ -60,84 +53,97 @@ class Hero(Entity):
     hero_right = PhotoImage(file = "images/hero-right.png")
 
 
-    def __init__(self):
+    def __init__(self, canvas):
+        super().__init__(canvas)
         self.hero = None
         self.x_cord = 0
         self.y_cord = 0
+        self.canvas = canvas
 
 
     def draw_hero(self):
         x = size / 2
         y = size / 2
-        self.hero = canvas.create_image(x, y, image = self.hero_down)
+        self.hero = self.canvas.create_image(x, y, image = self.hero_down)
 
 
     def move(self, x, y):
         self.x_cord += x 
         self.y_cord += y
-        canvas.move(self.hero, size * x , size * y)
+        self.canvas.move(self.hero, size * x , size * y)
 
 
     def image_update(self, new_image):
-        canvas.itemconfig(self.hero, image = new_image)
+        self.canvas.itemconfig(self.hero, image = new_image)
+
 
 class Skeleton(Entity):
-    skeleton_img = PhotoImage(file = "images/skeleton.png")
     
-    def __init__(self):
+    def __init__(self, canvas):
+        super().__init__(canvas)
         self.skeleton = None
         self.x_coord = 0
         self.y_coord = 0
-        
-    def random_coords(self):
-        self.x_coord = random.randint(0, 9)
-        self.y_coord = random.randint(0, 9)
+        self.skeleton_img = PhotoImage(file = "images/skeleton.png")
+
 
     def draw_skeleton(self):
-        self.skeleton = canvas.create_image( self.x_coord * size + size / 2, self.y_coord * size + size / 2, image = self.skeleton_img)
+        self.skeleton = self.canvas.create_image( self.x_coord * size + size / 2, self.y_coord * size + size / 2, image = self.skeleton_img)
 
 
+class Boss(Entity):
 
-tiled_map = Map() 
-tiled_map.draw_map()
-myhero = Hero()
-myhero.draw_hero()
-
-
-skeletons = []
-
-
-while len(skeletons) < 3: 
-    i =  0  
-    skeletons.append("skeleton") 
-    skeletons[i] = Skeleton()
-    skeletons[i].random_coords()
-    if tiled_map.get_cell(skeletons[i].x_coord, skeletons[i].y_coord) == True:
-        skeletons[i].draw_skeleton()
-        i += 1
-    elif tiled_map.get_cell(skeletons[i].x_coord == 0, skeletons[i].y_coord == 0):
-        del skeletons[i]
-    else:
-        del skeletons[i]
+    def __init__(self):
+        self.boss = None
+        self.x_coord = 0
+        self.y_coord = 0
+        self.boss_img = PhotoImage(file = "images/boss.png")
+    
+    def draw_boss(self):
+        self.boss = self.canvas.create_image( self.x_coord * size + size / 2, self.y_coord * size + size / 2, image = self.boss_img)
 
 
-def on_key_press(e):
-    if e.keysym == 'Up' and tiled_map.get_cell(myhero.x_cord, myhero.y_cord - 1) == True:
-        myhero.move(0, -1)
-        myhero.image_update(myhero.hero_up)
-    elif e.keysym == 'Down' and tiled_map.get_cell(myhero.x_cord, myhero.y_cord + 1) == True:
-        myhero.move(0, 1)
-        myhero.image_update(myhero.hero_down)
-    elif e.keysym == 'Left' and tiled_map.get_cell(myhero.x_cord - 1, myhero.y_cord) == True:
-        myhero.move(-1, 0)
-        myhero.image_update(myhero.hero_left)
-    elif e.keysym == 'Right' and tiled_map.get_cell(myhero.x_cord + 1, myhero.y_cord) == True:
-        myhero.move(1, 0)
-        myhero.image_update(myhero.hero_right)
+class Game(object):
 
-root.bind("<KeyPress>", on_key_press)
-canvas.pack()
-canvas.focus_set()
+    def __init__(self):
+        self.tiled_map = Map() 
+        self.tiled_map.draw_map()
+        self.myhero = Hero(self.tiled_map.canvas)
+        self.myhero.draw_hero()
+        self.skeletons = []
+        self.put_skeletons()
+        self.tiled_map.root.bind("<KeyPress>", self.on_key_press)
+        self.tiled_map.canvas.pack()
+        self.tiled_map.canvas.focus_set()
+        self.tiled_map.root.mainloop()
 
 
-root.mainloop()
+    def on_key_press(self, e):
+        if e.keysym == 'Up' and self.tiled_map.get_cell(self.myhero.x_cord, self.myhero.y_cord - 1) == True:
+            self.myhero.move(0, -1)
+            self.myhero.image_update(self.myhero.hero_up)
+        elif e.keysym == 'Down' and self.tiled_map.get_cell(self.myhero.x_cord, self.myhero.y_cord + 1) == True:
+            self.myhero.move(0, 1)
+            self.myhero.image_update(self.myhero.hero_down)
+        elif e.keysym == 'Left' and self.tiled_map.get_cell(self.myhero.x_cord - 1, self.myhero.y_cord) == True:
+            self.myhero.move(-1, 0)
+            self.myhero.image_update(self.myhero.hero_left)
+        elif e.keysym == 'Right' and self.tiled_map.get_cell(self.myhero.x_cord + 1, self.myhero.y_cord) == True:
+            self.myhero.move(1, 0)
+            self.myhero.image_update(self.myhero.hero_right)
+
+
+    def put_skeletons(self): 
+        i =  0  
+        while len(self.skeletons) < 3: 
+            self.skeletons.append("skeleton") 
+            self.skeletons[i] = Skeleton(self.tiled_map.canvas)
+            self.skeletons[i].random_coords()
+            if self.tiled_map.get_cell(self.skeletons[i].x_coord, self.skeletons[i].y_coord) == True:
+                self.skeletons[i].draw_skeleton()
+                i += 1
+            else:
+                del self.skeletons[i]
+
+
+game = Game()
