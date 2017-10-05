52c433fa754fd599bdd3fbc636933678b0eddcba
Borbala Dobo <dobori@gmail.com>
2017-10-05 11:35:13 +0200

    feat(classes.py): one skeleton can be on floor

--- week-05/project/classes.py
+++ week-05/project/classes.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 
 width = 720
@@ -80,11 +81,32 @@ class Hero(Entity):
     def image_update(self, new_image):
         canvas.itemconfig(self.hero, image = new_image)
 
+class Skeleton(Entity):
+    skeleton_img = PhotoImage(file = "images/skeleton.png")
+    
+    def __init__(self):
+        self.skeleton = None
+        self.x_coord = 0
+        self.y_coord = 0
+        
+    def random_coords(self):
+        self.x_coord = random.randint(0, 9)
+        self.y_coord = random.randint(0, 9)
+
+    def draw_skeleton(self):
+        self.skeleton = canvas.create_image( self.x_coord * size + size / 2, self.y_coord * size + size / 2, image = self.skeleton_img)
+        
+
 
 tiled_map = Map() 
 tiled_map.draw_map()
 myhero = Hero()
 myhero.draw_hero()
+skeleton1 = Skeleton()
+skeleton1.random_coords()
+if tiled_map.get_cell(skeleton1.x_coord, skeleton1.y_coord) == True:
+    skeleton1.draw_skeleton()
+
 
 
 def on_key_press(e):
@@ -103,7 +125,6 @@ def on_key_press(e):
 
 root.bind("<KeyPress>", on_key_press)
 canvas.pack()
-
 canvas.focus_set()
 
 
