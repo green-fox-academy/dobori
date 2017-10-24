324ed2428bb56f014712da5fa1c792fad3366a27
Borbala Dobo <dobori@gmail.com>
2017-10-24 15:38:01 +0200

    feat(draw-a-triangle.js): ready

---
+++ week-07/day-01/draw-triangle.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var lineCount = 4;
+
+// Write a program that draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+let star = "*";
+
+for (let i = 1; i <= lineCount; i++){
+    console.log(star.repeat(i));
+}
