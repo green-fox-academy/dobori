ff68397cb7af1000556de01b498e37009615821b
Borbala Dobo <dobori@gmail.com>
2017-10-24 12:08:05 +0200

    feat(define-basic-info.js): ready

---
+++ week-07/day-01/define-basic-info.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myNameIs = "Bori";
+let myAgeIs = 37;
+let myHeight = 1.56;
+let myStatus = false;
+
+console.log("My name is: " + myNameIs +", my age is: " + myAgeIs + ", my height is: " + myHeight + " and my married status is: " + myStatus);
