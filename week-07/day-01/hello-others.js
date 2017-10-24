3f705537d67ae2a4c2aa99c6378d7b43c537df41
Borbala Dobo <dobori@gmail.com>
2017-10-24 11:34:50 +0200

    feat(excersize 1-4): ready

---
+++ week-07/day-01/hello-others.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log("Hello, Valaki!");
+console.log("Hello, Masvalaki!");
+console.log("Hello, harmadik valaki!");
