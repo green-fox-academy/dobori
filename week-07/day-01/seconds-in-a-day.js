639e690f5d161c9478d68e8408ec490204a09779
Borbala Dobo <dobori@gmail.com>
2017-10-24 13:31:54 +0200

    feat(seconds-in-a-day.js): ready

---
+++ week-07/day-01/seconds-in-a-day.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var currentHours = 14;
+var currentMinutes = 34;
+var currentSeconds = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+
+let remainingSeconds = 24 * 60 * 60 - (currentHours * 60 * 60 + currentMinutes * 60 + currentSeconds);
+
+console.log(remainingSeconds);
