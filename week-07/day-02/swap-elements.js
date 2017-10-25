41d6580e3772ed9ec2fa5cfc5ebe7e37ce5dbfa6
Borbala Dobo <dobori@gmail.com>
2017-10-25 14:13:12 +0200

    feat(swap-elements.js): ready

---
+++ week-07/day-02/swap-elements.js
@@ -0,0 +1,11 @@
+'use strict';
+// - Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// - Swap the first and the third element of `abc`
+
+let abc = ["Arthur", "Boe", "Chloe"];
+
+var swap = abc[0];
+abc[0] = abc[2];
+abc[2] = swap;
+
+console.log(abc);
