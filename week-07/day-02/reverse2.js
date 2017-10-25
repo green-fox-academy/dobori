f6d962475420a52dece89b6110b869600783dcce
Borbala Dobo <dobori@gmail.com>
2017-10-25 16:35:23 +0200

    feat(reverse2.js): ready

---
+++ week-07/day-02/reverse2.js
@@ -0,0 +1,10 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+function reverseString(reversed) {
+    reversed = reversed.split("").reverse().join("");
+    console.log(reversed);
+}
+reverseString(reversed);
