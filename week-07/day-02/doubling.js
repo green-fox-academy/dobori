0dc10d3643b8bc86eb88b7170de8fb5f9c727277
Borbala Dobo <dobori@gmail.com>
2017-10-25 11:37:54 +0200

    feat(doubling.js): ready

---
+++ week-07/day-02/doubling.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Create a variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Log the result of `doubling(ak)` to the console
+
+let ak = 123;
+
+function doubling(ak) {
+    ak = ak * 2;
+    console.log(ak);
+  }
+  
+doubling(ak);
