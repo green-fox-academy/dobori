e9d30384375b4354f2a50eec94e43de7aa3bdf67
Borbala Dobo <dobori@gmail.com>
2017-11-08 15:15:01 +0100

    feat(backend): doubling part is ready

--- week-09/backend.js
+++ week-09/backend.js
@@ -3,10 +3,26 @@
 var express = require('express');
 var app = express ();
 
+express.json.type = "application/json"
 app.use('/assets', express.static('./assets'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/doubling', function(req, res){
+    var doubleStuff = req.query.input;
+    var toDouble = parseInt(doubleStuff);
+    if (!toDouble){
+        res.json({
+            "error": "Please provide an input!"
+        });
+    } else {
+        res.json({
+            "received": toDouble,
+            "result": toDouble * 2
+        } );
+    };
 });
 
 
