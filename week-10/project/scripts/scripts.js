c67250336c2390b0689ecd52d7ef297fa067b023
Borbala Dobo <dobori@gmail.com>
2017-11-13 22:53:31 +0100

    feat(fox player): design of the player is ready, timing and progress bar for the music is working

--- week-10/project/scripts/scripts.js
+++ week-10/project/scripts/scripts.js
@@ -6,7 +6,6 @@ function audioListPlay(){
 
         track = document.querySelectorAll("a");
         audioPlayer.src = track[0].href;
-        audioPlayer.play();
 
         for (let i=0; i<track.length; i++){
             track[i].addEventListener("click", function(e){
@@ -36,47 +35,52 @@ playsButton.addEventListener("click", function() {
             pauseButton.setAttribute('style', 'display: block;');
         }
 });
-        
+
+//Next Button
+var nextBtn = document.querySelector('#next_btn');
+
+nextBtn.addEventListener("click", function(){
 
 
+});
 
+//Prev Button
+
+      
+//function for timeline
+
+audioPlayer.addEventListener("timeupdate", function() {
+
+    let progressBar = document.querySelector('.progressbar_range');
+    let currentTime = audioPlayer.currentTime;
+        duration = audioPlayer.duration;    
+        progressBar.setAttribute('style', 'width:' + (currentTime + 0.25) / duration * 100 + '%' );
+
+});
+
+//show the times
+
+audioPlayer.addEventListener("timeupdate", function() {
+    let currentTimeDisplay = document.querySelector('#current_time');
+    let durationDisplay = document.querySelector('#timelenght');
+
+        duration = parseInt( audioPlayer.duration );
+        currentTime = parseInt( audioPlayer.currentTime );
+    
+        function timeFormatting(time){
+            let s = time % 60;
+            let m = Math.floor( time / 60 ) % 60;
+            
+                s = s < 10 ? "0" + s : s;
+                m = m < 10 ? "0"+ m : m;
+
+            let timestring = m + ":" + s;
+            return timestring;
+         }
+
+        currentTimeDisplay.innerText = timeFormatting(currentTime);
+        durationDisplay.innerText = timeFormatting(duration);
+    
+});
 
 audioListPlay();
-
-
-// var for audio content
-
-// var audio = document.querySelector("#audioPlayer");
-
-// html5 function - toggle play/pause btn and audio
-
-
-
-// function for timeline
-
-// audio.addEventListener("timeupdate", function() {
-//     var currentTime = audio.currentTime,
-//         duration = audio.duration,
-//         currentTimeMs = audio.currentTime * 1000;
-//     $('.progressbar_range').stop(true, true).animate({'width': (currentTime + .25) / duration * 100 + '%'}, 250, 'linear');
-// });
-
-
-// count function for timeleft
-
-// audio.addEventListener("timeupdate", function() {
-//     var timeleft = document.getElementById('timeleft'),
-//         duration = parseInt( audio.duration ),
-//         currentTime = parseInt( audio.currentTime ),
-//         timeLeft = duration - currentTime,
-//         s, m;
-    
-//     s = timeLeft % 60;
-//     m = Math.floor( timeLeft / 60 ) % 60;
-    
-//     s = s < 10 ? "0"+s : s;
-//     m = m < 10 ? "0"+m : m;
-    
-//     $('#timeleft').text("-"+m+":"+s);
-    
-// });
