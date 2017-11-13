e3dd0e69f9adb48051394eb7d35834aa6e605e7a
Borbala Dobo <dobori@gmail.com>
2017-11-13 17:29:03 +0100

    feat(fox player): own player are in, and pause, play button is working

--- week-10/project/scripts/scripts.js
+++ week-10/project/scripts/scripts.js
@@ -1,7 +1,9 @@
 
-function audioPlayer(){
+var audioPlayer = document.querySelector("#audioPlayer");
+
+function audioListPlay(){
     var currentSong = 0;
-    var audioPlayer = document.querySelector("#audioPlayer");
+
         track = document.querySelectorAll("a");
         audioPlayer.src = track[0].href;
         audioPlayer.play();
@@ -16,4 +18,65 @@ function audioPlayer(){
     
 }
 
-audioPlayer();
+
+var playsButton = document.querySelector('#plays_btn');
+
+playsButton.addEventListener("click", function() {
+
+    var playButton = document.querySelector('#play_btn');   
+    var pauseButton = document.querySelector('#pause_btn');
+
+        if (audioPlayer.paused == false) {
+            audioPlayer.pause();
+            playButton.setAttribute('style', 'display: block;');
+            pauseButton.setAttribute('style', 'display: none;');
+        } else {
+            audioPlayer.play();
+            playButton.setAttribute('style', 'display: none;');
+            pauseButton.setAttribute('style', 'display: block;');
+        }
+});
+        
+
+
+
+
+audioListPlay();
+
+
+// var for audio content
+
+// var audio = document.querySelector("#audioPlayer");
+
+// html5 function - toggle play/pause btn and audio
+
+
+
+// function for timeline
+
+// audio.addEventListener("timeupdate", function() {
+//     var currentTime = audio.currentTime,
+//         duration = audio.duration,
+//         currentTimeMs = audio.currentTime * 1000;
+//     $('.progressbar_range').stop(true, true).animate({'width': (currentTime + .25) / duration * 100 + '%'}, 250, 'linear');
+// });
+
+
+// count function for timeleft
+
+// audio.addEventListener("timeupdate", function() {
+//     var timeleft = document.getElementById('timeleft'),
+//         duration = parseInt( audio.duration ),
+//         currentTime = parseInt( audio.currentTime ),
+//         timeLeft = duration - currentTime,
+//         s, m;
+    
+//     s = timeLeft % 60;
+//     m = Math.floor( timeLeft / 60 ) % 60;
+    
+//     s = s < 10 ? "0"+s : s;
+//     m = m < 10 ? "0"+m : m;
+    
+//     $('#timeleft').text("-"+m+":"+s);
+    
+// });
