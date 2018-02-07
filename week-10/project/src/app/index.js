4db365184d54177e19f23f08b188acf3c4a6b45e
dobori <dobori@gmail.com>
2018-02-07 20:03:34 +0100

    feat(fox player): datas get from database via api

--- week-10/project/src/app/index.js
+++ week-10/project/src/app/index.js
@@ -1,20 +1,16 @@
 'use strict';
 
-import React from 'react';
-var ReactDOM = require('react-dom');
-var MyPlayListItem = require('./playlistitem');
-var AddItem = require('./additem');
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MyPlayListItem from './playlistitem';
+import AddItem from './additem';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import swal from 'sweetalert';
 require('./css/style.css');
 
-// { "id": 2, "title": "Music for programming", "system": 0},
-// { "id": 3, "title": "Driving", "system": 0},
-// { "id": 5, "title": "Fox house", "system": 0},
-// var tracks =[
-//     { "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "music/Ars_Sonor_-_02_-_Never_Give_Up.mp3" },
-//     { "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "music/Organoid_-_09_-_Purple_Drift.mp3" }
-// ]
+
+const playListUrl = "http://localhost:8080/playlists";
+const trackListUrl = "http://localhost:8080/playlist-tracks";
 
 class App extends React.Component{
     render(){
@@ -33,16 +29,27 @@ class MusicPlayer extends React.Component{
     constructor(props){
         super(props);
         this.state = { 
-            myPlayList: [{ "id": 2, "title": "Music for programming", "system": 0},
-                { "id": 3, "title": "Driving", "system": 0},
-                { "id": 5, "title": "Fox house", "system": 0}
-            ],
-            myTrackList: [{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "music/Ars_Sonor_-_02_-_Never_Give_Up.mp3" },
-                { "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "music/Organoid_-_09_-_Purple_Drift.mp3" }
-            ]
-        };
+            myPlayList: [],
+            myTrackList: []
+            };
         this.onDelete = this.onDelete.bind(this);
         this.onAdd = this.onAdd.bind(this);
+    }
+        
+    componentDidMount(){
+        fetch(playListUrl).then( data => data.json())        
+        .then( data => {
+            this.setState({
+                myPlayList: data
+            })
+        })
+        fetch(trackListUrl).then( trackdata => trackdata.json())        
+        .then(trackdata => {
+            console.log(trackdata)
+            this.setState({
+                myTrackList: trackdata
+            })
+        })
     }
 
     onDelete(item){
@@ -54,7 +61,7 @@ class MusicPlayer extends React.Component{
         }).then((isDeleted) => {
             if (isDeleted) {
                 var updatedPlayList = this.state.myPlayList.filter(function(val, index){
-                    return item !== val.title;
+                    return item !== val.playlist_title;
                 });
                 this.setState({
                     myPlayList: updatedPlayList
@@ -94,7 +101,7 @@ class MusicPlayer extends React.Component{
             }
             
             var updatedPlayList = this.state.myPlayList;
-            updatedPlayList.push({"id": 8, "title": inputValue, "system": 0});
+            updatedPlayList.push({"id": 8, "playlist_title": inputValue, "system": 0});
             this.setState({
                 myPlayList: updatedPlayList
             });
@@ -105,7 +112,7 @@ class MusicPlayer extends React.Component{
     render(){
         var myPlayList = this.state.myPlayList;
         myPlayList = myPlayList.map(function(item, index){
-            return(<MyPlayListItem key={index} item={item.title} onDelete={this.onDelete}/>
+            return(<MyPlayListItem key={index} item={item.playlist_title} onDelete={this.onDelete}/>
             );
         }.bind(this));
         var myTrackList = this.state.myTrackList;
